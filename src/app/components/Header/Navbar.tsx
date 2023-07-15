0de8c7be803a840f209702ca8f6fdf597e6afcf1
Dinh-Huy NGUYEN <dinh-huy@homeexchange.com>
2023-07-15 21:21:31 +0200

    D2 - Create header and routing system for dashboard

---
+++ src/app/components/Header/Navbar.tsx
@@ -0,0 +1,63 @@
+"use client"
+
+import Link from "next/link";
+import {AiOutlineMenu, AiOutlineClose} from "react-icons/ai";
+import {useState} from "react";
+
+const Navbar= () => {
+    const [nav, setNav] = useState(false);
+    const handleNav = () => {
+        setNav(!nav)
+    }
+    return (
+        <div className="fixed top-0 left-0 z-10 w-full duration-300 ease-in">
+            <div className="m-auto flex items-center justify-between p-4 text-white">
+                <Link href={"/"}>
+                    <h1 className="text-4xl font-bold">FirePhoenix</h1>
+                </Link>
+                <ul className="hidden sm:flex">
+                    <li className="p-4 hover:text-gray-500">
+                        <Link href={"/"}>Overview</Link>
+                    </li>
+                    <li className="p-4 hover:text-gray-500">
+                        <Link href={"/incomes"}>Incomes</Link>
+                    </li>
+                    <li className="p-4 hover:text-gray-500">
+                        <Link href={"/projects"}>Projects</Link>
+                    </li>
+                    <li className="p-4 hover:text-gray-500">
+                        <Link href={"/sales"}>Sales</Link>
+                    </li>
+                </ul>
+
+                {/* Mobile Button */}
+                <div onClick={handleNav} className="z-10 block sm:hidden">
+                    {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+                </div>
+
+                {/* Mobile Menu */}
+                <div className={
+                    nav ? 'sm:hidden absolute top-0 left-0 right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+                        : 'sm:hidden absolute top-0 left-[-100%] right-0 bottom-0 flex justify-center items-center w-full h-screen bg-black text-center ease-in duration-300'
+                }>
+                    <ul>
+                        <li className="p-4 text-4xl hover:text-gray-500">
+                            <Link href={"/"}>Overview</Link>
+                        </li>
+                        <li className="p-4 text-4xl hover:text-gray-500">
+                            <Link href={"/incomes"}>Incomes</Link>
+                        </li>
+                        <li className="p-4 text-4xl hover:text-gray-500">
+                            <Link href={"/projects"}>Projects</Link>
+                        </li>
+                        <li className="p-4 text-4xl hover:text-gray-500">
+                            <Link href={"/sales"}>Sales</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
---
+++ src/app/sales/page.tsx
@@ -0,0 +1,9 @@
+const SalesPage = () => {
+    return (
+        <>
+            <h1>Sales Page</h1>
+        </>
+    );
+};
+
+export default SalesPage;
